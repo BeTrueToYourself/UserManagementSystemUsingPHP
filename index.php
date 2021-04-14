782e69d8efc5e192d52fdc3e23d48bc383f27268
Ashwani Kumar <47925818+BeTrueToYourself@users.noreply.github.com>
2021-04-14 22:11:50 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,90 +1,99 @@
 <?php
 
-define('TITLE', "Login");
-include '../assets/layouts/header.php';
-check_logged_out();
+$conn = mysqli_connect("localhost", "root", "", "usermanagement");
+$results = mysqli_query($conn, "SELECT * FROM profile");
+$users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+body  {
+  background-image: url("userimage/bg.png");
+  background-color: #cccccc;
+}
+</style>
+</head>
+<body>
+<br>
+<center><img src="userimage/user.png" alt="userimage/bg.png" width="500" height="100"><center>
 
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php
+          	echo $_SESSION['success'];
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 
-<div class="container">
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    
+
+		<p><b>Welcome To The User Management System.....!!!! </b></P>
+		<br>
+		<p><em><b>Your Profile Name is </b></em><strong><?php echo $_SESSION['username']; ?></strong></p>
+	
+	<div class="container">
     <div class="row">
-        <div class="col-sm-4">
-
-        </div>
-        <div class="col-sm-4">
-            <form class="form-auth" action="includes/login.inc.php" method="post">
-
-                <?php insert_csrf_token(); ?>
-
-                <div class="text-center">
-                    <img class="mb-1" src="../assets/images/logo.png" alt="" width="130" height="130">
-                </div>
-
-                <h6 class="h3 mb-3 font-weight-normal text-muted text-center">Login to your Account</h6>
-
-                <div class="text-center mb-3">
-                    <small class="text-success font-weight-bold">
-                        <?php
-                            if (isset($_SESSION['STATUS']['loginstatus']))
-                                echo $_SESSION['STATUS']['loginstatus'];
-
-                        ?>
-                    </small>
-                </div>
-
-                <div class="form-group">
-                    <label for="username" class="sr-only">Username</label>
-                    <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
-                    <sub class="text-danger">
-                        <?php
-                            if (isset($_SESSION['ERRORS']['nouser']))
-                                echo $_SESSION['ERRORS']['nouser'];
-                        ?>
-                    </sub>
-                </div>
-
-                <div class="form-group">
-                    <label for="password" class="sr-only">Password</label>
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                    <sub class="text-danger">
-                        <?php
-                            if (isset($_SESSION['ERRORS']['wrongpassword']))
-                                echo $_SESSION['ERRORS']['wrongpassword'];
-                        ?>
-                    </sub>
-                </div>
-
-                <div class="col-auto my-1 mb-4">
-                    <div class="custom-control custom-checkbox mr-sm-2">
-                        <input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
-                        <label class="custom-control-label" for="rememberme">Remember me</label>
-                    </div>
-                </div>
-
-                <button class="btn btn-lg btn-primary btn-block" type="submit" value="loginsubmit" name="loginsubmit">Login</button>
-
-                <p class="mt-3 text-muted text-center"><a href="../reset-password/">forgot password?</a></p>
-
-                <p class="mt-4 mb-3 text-muted text-center">
-                    <a href="https://github.com/msaad1999/PHP-Login-System" target="_blank">
-                        Login System
-                    </a> | 
-                    <a href="https://github.com/msaad1999/PHP-Login-System/blob/master/LICENSE" target="_blank">
-                        MIT License
-                    </a>
-                </p>
+      <div class="col-4 offset-md-4" style="margin-top: 30px;">
+     
+       
+        <table class="table table-bordered">
+          
+          
+            <?php foreach ($users as $user): ?>
+				<thead>
+            <th>Photo</th>
+          </thead>
+		  <tbody>
+              <tr>
+                <td> <center><img src="<?php echo 'profileimg/' . $user['profile_image'] ?>" width="90" height="90" alt=""> </center></td>
                 
-            </form>
-        </div>
-        <div class="col-sm-4">
-
-        </div>
+              </tr>
+			  <thead>
+            <th>Story</th>
+          </thead>
+			  <tr> 
+			  <td><center><b><em><?php echo $user['bio']; ?></em> </b></center></td>
+			  </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+		<br>
+		<br>
+		<a href="editprofile.php" class="btn btn-success"><b>Click Here To Write A Story Please</b></a>
+      </div>
     </div>
+  </div>
+  <br>
+		<br>
+    	<p> <a href="index.php?logout='1'" style="color: red;"><b>Log Out</b></a> </p>
+    <?php endif ?>
 </div>
 
-
-<?php
-
-include '../assets/layouts/footer.php'
-
-?>
+</body>
+</html>
